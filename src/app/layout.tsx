09faf3c1f8ce6a2a8cdb509ee073609874589622
Carlos Guijarro López <c.guijarro@kynegos.com>
2025-09-16 13:57:05 +0000

    El titulo de la App será "Biomass"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Biomass Mapper',
+  title: 'Biomass',
   description: 'Find biomass sources near you.',
 };
 
