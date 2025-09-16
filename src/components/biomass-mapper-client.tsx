a00738b02a329d34020b3be5bb611b75a583cff5
Carlos Guijarro López <c.guijarro@kynegos.com>
2025-09-16 09:47:59 +0000

    quita el logo, es un desastre...

--- src/components/biomass-mapper-client.tsx
+++ src/components/biomass-mapper-client.tsx
@@ -11,7 +11,6 @@ import BiomassMap from '@/components/biomass-map';
 import InitialFilterDialog from '@/components/initial-filter-dialog';
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import MobilePanelToggle from "@/components/mobile-panel-toggle";
-import KynegosLogo from './kynegos-logo';
 
 export default function BiomassMapperClient() {
   const {
@@ -119,7 +118,6 @@ export default function BiomassMapperClient() {
             </Sheet>
         </div>
       </main>
-      <KynegosLogo />
       <InitialFilterDialog />
     </APIProvider>
   );
