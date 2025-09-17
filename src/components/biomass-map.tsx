32e059f051a016c0d186c423b0ea2d03e5c170e4
Caio Araujo Barbosa <c.araujo@kynegos.com>
2025-09-17 08:08:14 +0000

    Sigue sin funcionar la selección de ubicaciones cuando hago click, revís

--- src/components/biomass-map.tsx
+++ src/components/biomass-map.tsx
@@ -188,9 +188,8 @@ export default function BiomassMap() {
     lng: JSON.parse(selectedSource.geom_geojson).coordinates[0],
   } : null;
 
-  const handleClick = (e: { detail: { latLng: google.maps.LatLngLiteral | null; isMouseEvent: boolean; } }) => {
-    // Only handle pure clicks, not clicks that are part of a drag
-    if (!e.detail.isMouseEvent || !e.detail.latLng) {
+  const handleClick = (e: { detail: { latLng: google.maps.LatLngLiteral | null; } }) => {
+    if (!e.detail.latLng) {
       return;
     }
 
