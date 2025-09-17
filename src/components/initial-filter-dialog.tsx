c1310a891de9095c5af39c43b953719204f9dd1b
Carlos Guijarro López <c.guijarro@kynegos.com>
2025-09-17 10:52:47 +0000

    Algo no está bien:

--- src/components/initial-filter-dialog.tsx
+++ src/components/initial-filter-dialog.tsx
@@ -11,7 +11,7 @@ import {
 } from '@/components/ui/dialog';
 import { useTranslation } from '@/hooks/use-translation';
 import { Button } from './ui/button';
-import Image from 'next/image';
+import { Icons } from './icons';
 
 export default function InitialFilterDialog() {
   const { t } = useTranslation();
@@ -21,13 +21,7 @@ export default function InitialFilterDialog() {
     <Dialog open={isInitialDialogOpen} onOpenChange={setIsInitialDialogOpen}>
       <DialogContent className="sm:max-w-md">
         <DialogHeader>
-          <Image 
-            src="/kynegos-logo.svg" 
-            alt="Kynegos Logo" 
-            width={180} 
-            height={40} 
-            className="mx-auto mb-4" 
-          />
+          <Icons.kynegos className="w-48 h-auto mx-auto mb-4" />
           <DialogTitle className="text-2xl font-bold text-center text-primary">
             {t('app_title')}
           </DialogTitle>
