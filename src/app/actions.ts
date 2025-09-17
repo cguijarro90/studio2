7eede9c05dc4de2991060e2d24a9289c08d5d92b
Carlos Guijarro López <c.guijarro@kynegos.com>
2025-09-17 15:06:28 +0000

    Vamos a hacer una cosa, de momento con los campos que hemos mapeado es s

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -47,7 +47,7 @@ export async function searchBiomass(
       objectid as id,
       descripcion as name,
       'otros' as type,
-      POTENCIA_INSTALADA as quantity,
+      0 as quantity,
       location,
       ST_DISTANCE(location, ST_GEOGPOINT(@lng, @lat)) as distance_m
     FROM data
