93cc7e21c5e2c21a0492868683859cc59a5eb9cf
Carlos Guijarro López <c.guijarro@kynegos.com>
2025-09-19 09:01:47 +0000

    Vamos a ampliar el límite a 10000

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -134,7 +134,7 @@ export async function searchAgriculturalPlots(
       ST_ASGEOJSON(geometry) as geometry
     FROM ${table}
     WHERE ST_DWITHIN(geometry, ST_GEOGPOINT(@lng, @lat), @radius_m)
-    LIMIT 5000
+    LIMIT 10000
   `;
 
   const queryParams = {
@@ -189,7 +189,7 @@ export async function searchForestPlots(
         ST_ASGEOJSON(geometry) as geometry
       FROM ${table}
       WHERE ST_DWITHIN(geometry, ST_GEOGPOINT(@lng, @lat), @radius_m) AND SP1 IS NOT NULL
-      LIMIT 5000
+      LIMIT 10000
     `;
   
     const queryParams = {
