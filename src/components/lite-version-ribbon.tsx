71662ce58fd2fd5450b58a682d68dec5f9d6d09c
Carlos Guijarro López <c.guijarro@kynegos.com>
2025-10-02 09:42:17 +0000

    ¿podríamos mover el ribbon a esa ubicación?

--- src/components/lite-version-ribbon.tsx
+++ src/components/lite-version-ribbon.tsx
@@ -14,14 +14,13 @@ export default function LiteVersionRibbon() {
 
   return (
     <div className={cn(
-      "absolute top-14 left-0 z-10",
+      "absolute top-4 left-1/2 -translate-x-1/2 z-10",
       "h-8 flex items-center justify-center",
       "bg-primary text-primary-foreground",
-      "px-4 shadow-lg",
-      "transform -translate-x-8 translate-y-8 -rotate-45"
+      "px-4 shadow-lg rounded-md"
     )}>
       <div className={cn(
-        "w-40 text-center text-sm font-bold",
+        "text-center text-sm font-bold",
       )}>
         {t('lite_version' as any)}
       </div>
